dca44630de8e7ee267928f62f14dc71f4b79604d
Admin <admin@Admins-MacBook-Air.local>
2021-02-17 23:08:03 -0600

    pruebas al componente gifGridItems

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,12 +3,33 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('probar compente de GifGrid', () => {
+    const title = 'titulo';
+    const url='https://..';
+    const wrapper = shallow( <GifGridItem title={title} url={url}/>);
+
     test('mostrar el componente correctamente', () => {
-        const title = 'titulo';
-        const url='https://..';
-        const wrapper = shallow( <GifGridItem title={title} url={url}/>);
 
         expect(wrapper).toMatchSnapshot();
-    })
+
+    });
+    
+    test('Debe de tener un parrafo con el title', () => {
+       const p = wrapper.find('p');
+       expect(p.text()).toBe(title); 
+    });
+
+    test('debe de tener la imagen igual al src y alt de los props', () => {
+       const img = wrapper.find('img');
+
+       expect(img.prop('src')).toBe(url);
+       expect(img.prop('alt')).toBe(title); 
+    });
+    
+    test('debe de tener la clase animate__fadeInTopLeft', () => {
+       const div = wrapper.find('div');
+       const className = div.prop('className');
+   
+       expect(className.includes('animate__fadeInTopLeft')).toBe(true);
+    });
     
 })
