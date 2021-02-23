ff74b49b956e28513601108ed247fbc33d5be362
Alejandro <alejandro@messoft.com>
2021-02-23 11:28:51 -0600

    prueba hook

---
+++ src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,15 @@
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from '@testing-library/react-hooks';
+
+describe('Pruebas en el hook useFetchGifs', () => {
+
+    test('debe de retornar el estado inicial', () => {
+        const {result} = renderHook( () => useFetchGifs('One Punch') );
+        const { data, loading } = result.current;
+
+        // console.log(data, loading );
+        expect(data).toEqual([]);
+        expect(loading).toBe(true);
+    });
+    
+});
